66d252a637622f4b0a2b51c06971def5576fd864
tevoshw <tevoshw@gmail.com>
2026-01-21 15:18:05 -0300

    fix some bugs and finisht the project

--- bank/services.py
+++ bank/services.py
@@ -1,9 +1,15 @@
 from bank.validation import Validation
+import sqlite3
 
 class Services:
     def __init__(self):
         self.validation = Validation()
+        self.conn = sqlite3.connect("bank/database.db")
+        self.cursor = self.conn.cursor()
 
+
+# ------------------------------------------------------- ! ------------------------------------------
+# ---------------------------- ANALISAR ESCOLHA DO USUARIO --------------------------------------
     def main_method_connect(self, choose):
 
         # VERIFICA QUAL OPÇÃO O USUÁRIO ESCOLHEU E REDIRECIONA PARA A FUNÇÃO JÁ
@@ -14,46 +20,35 @@ class Services:
                 self.create_account()
 
             case "2": # LOGIN
-                self.login()
+                if_login = self.login()
+                if if_login == True:
+                    return True
 
             case "3": # SAIR
                 print("Encerrando programa!")
 
+# --------------------------------------------------- ! ----------------------------------------------------
+# ----------------------------- LOGIN LÓGICA -----------------------------------------------
 
     # OBTER OS DADOS DA CONTA E CRIAR ELA, REDIRECIONA PARA A PAGINA DE VALIDAR O USER E PASSWORD
     def create_account(self):
 
         # PEGAR OS DADOS
-        username = input("Digite seu usuário: ")
+        self.username = input("Digite seu usuário: ")
         password = int(input("Digite sua senha: "))
         
         # INSTANCIAR A CLASSE DA LÓGICA DE CRIAR CONTA
-        self.validation.create_account(username, password)
+        self.validation.create_account(self.username, password)
 
 
 
     def login(self):
 
         # PEGAR OS DADOS
-        username = input("Digite seu usuário: ")
+        self.username = input("Digite seu usuário: ")
         password = int(input("Digite sua senha: "))
 
         # INSTANCIAR A CLASSE E USAR SUAS FUNÇÕES DA LÓGICA DE LOGIN DA CONTA
-        self.validation.verify_login(username, password)
-
-
-                
-    def main_to_do_accountbank(self):
-            pass
-
-
-        
-
-    def deposit(self):
-       pass # VER E DEPOSITAR
-                
-    def withdrawaw(self):
-        pass # VER E SACAR
-
-    def view_balance(self):
-          pass # SÓ VER O DINHEIRO
+        if_login = self.validation.verify_login(self.username, password)
+        if if_login == True:
+            return True
