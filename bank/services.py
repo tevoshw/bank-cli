76b0de5674e2e85f82335ac6dd76ddb8de7444ff
tevoshw <tevoshw@gmail.com>
2026-01-20 23:15:11 -0300

    fix bug logic and add some adds

--- bank/services.py
+++ bank/services.py
@@ -1,28 +1,53 @@
+from bank.validation import Validation
+
 class Services:
-    def __init__(self, choose):
-        self.choose: str = str(choose)
+    def __init__(self):
+        self.validation = Validation()
 
+    def main_method_connect(self, choose):
+        self.choose = str(choose)
 
-    def main(self):
         match self.choose:
-            case "1":
-                pass
-            case "2":
-                pass
-            case "2":
-                pass
-            case "3":
-                pass
+            case "1": # CRIAR CONTA
+                self.create_account()
+            case "2": # LOGIN
+                self.login()
+            case "3": # SAIR
+                print("Encerrando programa!")
 
 
+    
     def create_account(self):
-                    pass
+
+        # PEGAR OS DADOS
+        username = input("Digite seu usuário: ")
+        password = int(input("Digite sua senha: "))
+        
+        # INSTANCIAR A CLASSE DA LÓGICA DE CRIAR CONTA
+        self.validation.create_account_verify_user(username, password)
 
     def login(self):
-                    pass
+        # PEGAR OS DADOS
+        username = input("Digite seu usuário: ")
+        password = int(input("Digite sua senha: "))
+
+        
+        # INSTANCIAR A CLASSE E USAR SUAS FUNÇÕES DA LÓGICA DE LOGIN DA CONTA
+        self.validation.verify_login(username, password)
+
+
+                
+    def main_to_do_accountbank(self):
+            pass
+
+
+        
 
     def deposit(self):
-                    pass
+       pass # VER E DEPOSITAR
                 
     def withdrawaw(self):
-                    pass
+        pass # VER E SACAR
+
+    def view_money(self):
+          pass # SÓ VER O DINHEIRO
